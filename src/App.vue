7d966db3820b871559b000f81187c6f1439b77f0
Gustavo Lima <limagustavorafael@hotmail.com>
2022-12-11 14:12:26 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="columns is-flex is-flex-direction-column" id="app">
+
+    <div class="column ">
+      <nav class="navbar is-align-items-center is-light is-flex" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <router-link to="/landing" class="navbar-item"> <img src="./assets/imagens/Logo-uni.png" alt="Logo" /> </router-link>
+
+        </div>
+
+
+        <div class="campo-pesquisa">
+          <form class='search-bar'>
+            <input placeholder="Pesquisar" />
+            <button type='submit'><img src='./assets/imagens/Union.png' /></button>
+          </form>
+        </div>
+
+
+
+        <div class="navbar-end">
+          <div class='navbar-item'>
+            <router-link to="/about" class="navbar-item">Entrar</router-link>
+          </div>
+        </div>
+
+
+      </nav>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,138 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <section class="column ">
+    <div class='destaques-box'>
+      <h1 class="title destaques">Destaques</h1>
+    </div>
+
+    <div class="destaque-cards is-flex is-align-items-center is-justify-content-center">
+      <div class="card is-flex is-flex-direction-column is-align-items-center">
+        <div class="blue-triangle-top"></div>
+
+        <img class="card-icon mt-5" src="../assets/imagens/livro.png" alt="Logo" />
+        <div class="card-titulo">
+          <h4>Dou aulas de inglês</h4>
+
+
+      
+
+        </div>
+        <div class="title-line"></div>
+        <div class="card-content pt-1">
+          <div class="content has-text-centered is-flex is-align-items-center">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at
+            eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras
+            mattis consectetur purus sit amet fermentum.
+          </div>
+          <div class="user-card-information is-flex is-align-items-center">
+            <figure class="image is-64x64">
+              <img class="is-rounded" src="../assets/imagens/perfil.png" alt="Logo" />
+            </figure>
+            <div class="user-card-name ml-2 is-flex is-flex-direction-column is-align-items-flex-start">
+              <h1> Otavio Augusto</h1>
+              <small>Engenharia de Software</small>
+            </div>
+          </div>
+
+        </div>
+        <div class="green-triangle-bottom"></div>
+
+
+      </div>
+    </div>
+    
+    <!-- Cartegorias  -->
+
+    <div class="column is-flex is-flex-direction-row is-justify-content-space-evenly mt-6">
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/livro.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Aulas</p>
+      </div>
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/iconeComida.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Alimentos</p>
+      </div>
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/artesanato.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Artesanatos</p>
+      </div>
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/servicos.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Serviços</p>
+      </div>
+
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/manutencao.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Manutenção</p>
+      </div>
+      <div class="categoria level-item has-text-centered is-flex-direction-column">
+        <figure class="image is-64x64">
+          <img src="../assets/imagens/outros.png" alt="Logo" />
+        </figure>
+        <p class='cartegoria-titulo'>Outros</p>
+      </div>
+
+    </div>
+
+    <!-- Boxes  -->
+
+    <div class="columns is-flex is-flex-direction-column	">
+      <div class="column">
+
+        <div class="aulas box">
+          <div class='destaques-box'>
+            <h1 class="title destaques">Aulas</h1>
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+      <div class="column">
+
+        <div class="aulas box">
+          <div class='destaques-box'>
+            <h1 class="title destaques">Alimenos</h1>
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+
+
+      <div class="column">
+
+        <div class="aulas box">
+          <div class='destaques-box'>
+            <h1 class="title destaques">Artesanatos</h1>
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+
+  </section>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
-</script>
